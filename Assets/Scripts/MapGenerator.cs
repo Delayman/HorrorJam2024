03a229ee05d4bc96e5b3bee144b2fa25445c65e2
Delayman <61342646+Delayman@users.noreply.github.com>
2024-10-12 15:05:13 +0700

    Infinite map generator

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject[] titlePrefabs;
+    public float zSpawn = 0f;
+    public float tileLength = 30f;
+    public int tileCount = 5;
+    public int tileDeletionDelay;
+
+    public Transform player;
+
+
+    private List<GameObject> activeTiles = new List<GameObject>();
+
+    private void Start()
     {
-        
+        for(int i = 0;i < tileCount; i++)
+        {
+            if(i == 0)
+            {
+                SpawnMap(0);
+            }else
+            {
+                SpawnMap(Random.Range(0, titlePrefabs.Length));
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update() 
     {
-        
+        if(player.position.z - tileDeletionDelay  > zSpawn - (tileCount * tileLength))
+        {
+            SpawnMap(Random.Range(0, titlePrefabs.Length));
+            DeleteTile();
+        }
+    }
+
+    public void SpawnMap(int _index)
+    {        
+        GameObject tile = Instantiate(titlePrefabs[_index],transform.forward * zSpawn, transform.rotation);
+        activeTiles.Add(tile);
+
+        zSpawn += tileLength;
+    }
+
+    private void DeleteTile()
+    {
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
     }
 }
