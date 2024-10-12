10213969b551811b587f43d65850f696fe521c9a
Delayman <61342646+Delayman@users.noreply.github.com>
2024-10-12 12:17:26 +0700

    PlayerMovement
    Forward and Lane Switch

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]private float _moveSpeed;
+    [SerializeField]private float _laneMovedDistance = 2;
+    
+
+    private CharacterController _characterController;
+    private Vector3 _direction;
+
+    //0L 1M 2R
+    private int currentLane = 1;
+
+    private void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update() 
+    {
+        _direction.z = _moveSpeed;
+
+        if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if(currentLane < 2) {currentLane++;}
+        }
+
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if(currentLane > 0) {currentLane--;}
+        }
+
+        Vector3 targetPos = transform.position.z * transform.forward + transform.position.y * transform.up;
+
+        switch (currentLane)
+        {
+            case 0: targetPos += Vector3.left * _laneMovedDistance; break;
+            case 2: targetPos += Vector3.right * _laneMovedDistance; break;
+        }
+
+        if(transform.position != targetPos) 
+        {
+            Vector3 diff = targetPos - transform.position;
+            Vector3 moveDir = diff.normalized * 10f * Time.deltaTime;
+            if(moveDir.sqrMagnitude < diff.magnitude)
+            {
+                _characterController.Move(moveDir);
+            }else
+            {
+                _characterController.Move(diff);
+            }
+        }
+
+        _characterController.Move(_direction * Time.deltaTime);
+    }
+}
