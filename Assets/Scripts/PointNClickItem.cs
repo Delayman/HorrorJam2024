749aa75a99312cd6f6252b5cc327d0a7826d48e9
Delayman <61342646+Delayman@users.noreply.github.com>
2024-10-17 21:46:16 +0700

    80% PointNClick
    Missing lore text then we're done

--- Assets/Scripts/PointNClickItem.cs
+++ Assets/Scripts/PointNClickItem.cs
@@ -7,6 +7,10 @@ public class PointNClickItem : MonoBehaviour
     public GameObject[] itemDataUi;
     public GameObject itemFrame;
     public GameObject itemFrameBtn;
+    public GameObject itemFrameFallenBtn;
+    public GameObject key;
+
+    private bool isPickUpKey;
 
     public void OnClickItem_1()
     {
@@ -17,25 +21,42 @@ public class PointNClickItem : MonoBehaviour
     public void OnClickItem_2()
     {
         itemDataUi[1].SetActive(true);
+        itemDataUi[1].GetComponent<Animator>().SetBool("IsUp", true);
     }
 
     public void OnClickItem_3()
     {
-        itemDataUi[2].SetActive(true);
+        
     }
 
     public void OnClickItem_4()
     {
         itemDataUi[3].SetActive(true);
+        itemDataUi[3].GetComponent<Animator>().SetBool("IsUp", true);
+
+        if(!isPickUpKey) return;
+
+        itemDataUi[4].SetActive(true);
+        itemDataUi[4].GetComponent<Animator>().SetBool("IsUp", true);
+        
+    }
+
+    public void OnClickItem_Key()
+    {
+        itemDataUi[5].SetActive(true);
+        itemDataUi[5].GetComponent<Animator>().SetBool("IsUp", true);
+        isPickUpKey = true;
+        key.SetActive(false);
     }
 
     //case picked up key
 
     public void OnClickItem_Sp()
     {
-        Debug.Log("huh");
         itemFrame.GetComponent<Rigidbody>().useGravity = true;
+        itemDataUi[2].GetComponent<Animator>().SetBool("IsUp", true);
         itemFrameBtn.SetActive(false);
+        itemFrameFallenBtn.SetActive(true);
     }
 
     public void OnClickOk()
